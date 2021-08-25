636f1e1f0a47dd758d19758b0095124042569dd0
Lokesh pemmanaboina <84330548+lokesh182002@users.noreply.github.com>
2021-08-25 22:15:07 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,11 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] =round(float(sum (scores)/3),2)
+    query_name = input()
+print(student_marks[name])    
